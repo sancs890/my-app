554c3025e2a5ae043a06a98bd2d53d372cc5f686
Sanjeevakumar Alagur <sanjeevakumar.alagur@oracle.com>
2023-06-23 14:16:20 +0530

    Added numbering for the books. Added CSS for the same.

---
+++ src/books.js
@@ -0,0 +1,23 @@
+import img1 from './images/book_image.jpg'
+import img2 from './images/book2_image.jpg'
+import img3 from './images/book3_image.jpg'
+export const books = [
+  {
+    title: 'Iron Flame (The Empyrean, 2)',
+    author: 'Rebecca Yarros',
+    img: img1,
+    id: 1,
+  },
+  {
+    author: 'Rebecca Yarros ',
+    title: 'Fourth Wing (The Empyrean, 1)',
+    img: img2,
+    id: 2,
+  },
+  {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: img3,
+    id: 3,
+  },
+]
--- src/index.js
+++ src/index.js
@@ -1,49 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-
-const books = [
-  {
-    title: 'Iron Flame (The Empyrean, 2)',
-    author: 'Rebecca Yarros',
-    img: './images/book_image.jpg',
-    id: 1,
-  },
-  {
-    author: 'James Clear',
-    title: 'Atomic Habits',
-    img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
-    id: 2,
-  },
-]
-
+import { books } from './books'
+import Book from './Book'
 const BookList = () => {
-  const someValue = 'shakeAndBake'
-  const getBook = (id) => {
-    const book = books.find((book) => book.id === id)
-    console.log(book)
-  }
   return (
     <section className="booklist">
-      {books.map((book) => {
-        return <Book {...book} key={book.id} getBook={getBook} />
+      {books.map((book, index) => {
+        return <Book {...book} key={book.id} number={index} />
       })}
     </section>
-  )
-}
-
-const Book = ({ author, title, img, id, getBook }) => {
-  const getSingleBook = () => {
-    getBook(id)
-  }
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <button onClick={getSingleBook}>Click Me</button>
-      <button onClick={() => getBook(id)}>Click Me</button>
-      <h4>{author.toUpperCase()}</h4>
-    </article>
   )
 }
 
